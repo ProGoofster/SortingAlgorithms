c11468ad5a1ea26b8687c008e0d7cb4783c23bbe
ProGoofster <dragons4me@comcast.net>
2025-02-06 20:42:28 -0600

    add 10-sorted data instead of random

--- src/main/java/org/example/Tester.java
+++ src/main/java/org/example/Tester.java
@@ -14,10 +14,13 @@ public class Tester {
     // to sort the array.
     public double singleTest(int size){
         int[] arr = new int[size];
-        Random ran = new Random();
-        for(int i = 0; i < size; i++){
-            arr[i] = ran.nextInt();
-        }
+//        Random ran = new Random();
+//        for(int i = 0; i < size; i++){
+//            arr[i] = ran.nextInt();
+//        }
+
+        generateKSorted(arr, 10);
+
         double startTime = System.nanoTime();
         sa.sorty(arr);
         double endTime = System.nanoTime();
@@ -38,6 +41,39 @@ public class Tester {
 
         System.out.println(sa.getClass().getSimpleName()+ " sorting finished with " + iterations + " iterations and " + size+ " size");
         return (totalTime/iterations)/1000000;
+    }
+
+    //We say that an array A is k-sorted if it has the property that for every
+    //element i in array A, i’s position in array A is at most k away from i’s position in a sorted array of the same
+    //data. For example, the array: [1, 4, 2, 3, 5, 7, 6, 8] is 2-sorted because every element is within 2 positions
+    //of its “correct” position in a sorted array. Any array that is k-sorted is also j-sorted for all j > k.
+    //In this part of the assignment, you will study the question of whether it is faster to sort k-sorted data than
+    //fully random data. You will need to write a method generateKSorted(my_array) that fills the array that is
+    //passed as an argument with 10-sorted data (each element is within 10 positions of its correct position in the
+    //array).
+    //Repeat the performance experiments from problem 5, this time with 10-sorted data instead of random data.
+
+    public void generateKSorted(int[] arr, int k) {
+        int n = arr.length;
+
+        // Fill array with sorted values
+        for (int i = 0; i < n; i++) {
+            arr[i] = i;
+        }
+
+        Random rand = new Random();
+
+        // Shuffle elements within k positions
+        for (int i = 0; i < n; i++) {
+            int swapIndex;
+            do {
+                swapIndex = i + rand.nextInt(2 * k + 1) - k; // Choose a position within k range
+            } while (swapIndex < 0 || swapIndex >= n);
+
+            int temp = arr[i];
+            arr[i] = arr[swapIndex];
+            arr[swapIndex] = temp;
+        }
     }
 
 }
